c21f27dbddd9d7dbc6cfb7cb97a46a153666cab1
cheRoot <diemason@list.ru>
2017-04-24 21:21:55 +0000

    collections: edit collectionsProcessTiming

--- chapter_004/src/main/java/ru/chedmitriy/collections/mapExample/User.java
+++ chapter_004/src/main/java/ru/chedmitriy/collections/mapExample/User.java
@@ -1,13 +1,31 @@
 package ru.chedmitriy.collections.mapExample;
 
 /**
-*
-*/
+ * Главный класс,здесь
+ * задаются параметры
+ * для каждого нового User
+ */
 public class User {
+    /**
+     * имя объекта
+     * */
     private final String name;
+    /**
+     * идентификатор объекта
+     * */
     private final int id;
+    /**
+     * город объекта
+     * */
     private final String city;
-
+/**
+ * Конструктор,где задаются общие для всех объектов
+ * данного класса параметры
+ * @param name - имя объекта USer
+ * @param id - идентификатор объекта User
+ * @param city  -  город объекта
+ *
+ * */
     public User(int id,String name,String city) {
         this.id = id;
         this.name = name;
