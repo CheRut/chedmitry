addf7119210183c5880247738368524455307b2f
CheRut <diemason@list.ru>
2017-08-24 23:51:10 +0300

    добавлен метод add(E e) в интерфейс SimpleTree в пакете tree; исправлен IteratorExample в пакете IteratorAndDoubleArray.iteratorForDubArray.

--- chapter_005/src/main/java/ru/chedmitriy/collectionsPro/tree/SimpleTree.java
+++ chapter_005/src/main/java/ru/chedmitriy/collectionsPro/tree/SimpleTree.java
@@ -5,4 +5,5 @@ package ru.chedmitriy.collectionsPro.tree;
  */
 public interface SimpleTree< E extends Comparable<E>> extends Iterable<E> {
     boolean add(E parent,E child);
+    boolean add (E e);
 }
